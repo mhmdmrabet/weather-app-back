051e4358e35b34b6a1e010451e9c3b566fb68a42
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-06 22:46:16 +0100

    Fix uuid table users

--- database/migrations/1646486970127_users.ts
+++ database/migrations/1646486970127_users.ts
@@ -6,7 +6,7 @@ export default class UsersSchema extends BaseSchema {
   public async up() {
     await this.raw('create extension IF NOT EXISTS "uuid-ossp" schema pg_catalog version "1.1";');
     this.schema.createTable(this.tableName, (table) => {
-      table.increments("id").primary();
+      table.uuid("id").primary().defaultTo(this.db.rawQuery("uuid_generate_v4()").knexQuery);
       table.string("email", 255).notNullable();
       table.string("password", 180).notNullable();
       table.string("remember_me_token").nullable();
