97ed8399395642968c9606bb532889664b551157
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-07 21:20:30 +0100

    Add login and logout routes

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -4,15 +4,26 @@ import RegisterValidator from "App/Validators/RegisterValidator";
 
 export default class AuthController {
   public async register({ auth, request, response }: HttpContextContract) {
+    const payload = await request.validate(RegisterValidator);
+    await User.create(payload);
+    const token = await auth
+      .use("api")
+      .attempt(payload.email, payload.password, { expiresIn: "6h" });
+    return response.created(token);
+  }
+
+  public async login({ request, response, auth }: HttpContextContract) {
+    const { email, password } = request.only(["email", "password"]);
     try {
-      const payload = await request.validate(RegisterValidator);
-      await User.create(payload);
-      const token = await auth
-        .use("api")
-        .attempt(payload.email, payload.password, { expiresIn: "2days" });
-      return response.created(token);
+      const token = await auth.use("api").attempt(email, password, { expiresIn: "6h" });
+      return token;
     } catch (error) {
-      return response.badRequest();
+      return response.badRequest("Invalid credentials");
     }
+  }
+
+  public async logout({ auth, response }: HttpContextContract) {
+    await auth.use("api").revoke();
+    return response.noContent();
   }
 }
--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -3,5 +3,7 @@ import Route from "@ioc:Adonis/Core/Route";
 export default function authRoutes() {
   Route.group(() => {
     Route.post("register", "AuthController.register").as("auth.register");
+    Route.post("login", "AuthController.login").as("'auth.login");
+    Route.delete("logout", "AuthController.logout").as("auth.logout");
   });
 }
