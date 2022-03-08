ce3b327df05cb91b30d0f1abd8f81a2772c82224
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-08 14:35:21 +0100

    Refactor

--- app/Controllers/Http/LocationsController.ts
+++ app/Controllers/Http/LocationsController.ts
@@ -1,32 +1,8 @@
-import { inject } from "@adonisjs/fold";
-import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Location from "App/Models/Location";
-import LocationService from "App/Services/LocationService";
-import { Location as LocationType } from "App/types/location.type";
 
-@inject()
 export default class LocationsController {
-  public locationService = LocationService;
-
   public async index() {
     const locations = await Location.query().orderBy("updated_at", "desc");
     return locations;
-  }
-
-  public async storeLocation({ params }: HttpContextContract) {
-    try {
-      const locationData: LocationType[] = await this.locationService.findLocation(params.city);
-      if (locationData.length > 0) {
-        const { name, lat, lon, country, state } = locationData[0];
-        const searchPayload = { name };
-        const savePayload = { name, lat, lon, state, country };
-        const storedLocation = await Location.firstOrCreate(searchPayload, savePayload);
-        return storedLocation;
-      } else {
-        throw new Error("Location not found");
-      }
-    } catch (error) {
-      throw error;
-    }
   }
 }
--- app/Services/LocationService.ts
+++ app/Services/LocationService.ts
@@ -1,4 +1,6 @@
 import Env from "@ioc:Adonis/Core/Env";
+import { Location as LocationType } from "App/types/location.type";
+import Location from "App/Models/Location";
 import axios from "axios";
 
 class LocationService {
@@ -9,6 +11,23 @@ class LocationService {
     try {
       const response = await axios.get(url);
       return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async storeLocation(city: string) {
+    try {
+      const locationData: LocationType[] = await this.findLocation(city);
+      if (locationData.length > 0) {
+        const { name, lat, lon, country, state } = locationData[0];
+        const searchPayload = { name };
+        const savePayload = { name, lat, lon, state, country };
+        const storedLocation = await Location.firstOrCreate(searchPayload, savePayload);
+        return storedLocation;
+      } else {
+        throw new Error("Location not found");
+      }
     } catch (error) {
       throw error;
     }
