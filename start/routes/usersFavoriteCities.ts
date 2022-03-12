89ef40b084ae96f407511869a8d641a24330d27d
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-12 12:49:39 +0100

    Add params matcher

--- start/routes/usersFavoriteCities.ts
+++ start/routes/usersFavoriteCities.ts
@@ -3,10 +3,12 @@ import Route from "@ioc:Adonis/Core/Route";
 export default function usersFavoriteCitiesRoutes() {
   Route.group(() => {
     Route.get("", "UsersFavoriteCitiesController.index").as("usersFavoriteCities.index");
-    Route.post("/:cityId", "UsersFavoriteCitiesController.attach").as("usersFavoriteCities.attach");
-    Route.delete("/:cityId", "UsersFavoriteCitiesController.dettach").as(
-      "usersFavoriteCities.dettach"
-    );
+    Route.post("/:cityId", "UsersFavoriteCitiesController.attach")
+      .where("cityId", Route.matchers.number())
+      .as("usersFavoriteCities.attach");
+    Route.delete("/:cityId", "UsersFavoriteCitiesController.dettach")
+      .where("cityId", Route.matchers.number())
+      .as("usersFavoriteCities.dettach");
   })
     .prefix("/users/cities")
     .middleware("auth");
