8466236620c82d98b943d4d89a27802b44754b42
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-12 17:14:55 +0100

    Transform data before send it

--- app/Controllers/Http/WeatherController.ts
+++ app/Controllers/Http/WeatherController.ts
@@ -10,11 +10,19 @@ export default class WeathersController {
 
   public async getWeatherByName({ params }: HttpContextContract) {
     try {
-      const location = await this.locationService.storeLocation({
+      const { id, name } = await this.locationService.storeLocation({
         city: params.city,
       });
-      const weather = this.weatherService.getWeatherByName(location.name);
-      return weather;
+      const weather = await this.weatherService.getWeatherByName(name);
+
+      return {
+        location: { id, name },
+        weather: {
+          icon: weather.weather[0].icon,
+          temperature: weather.main.temp,
+          description: weather.weather[0].description,
+        },
+      };
     } catch (error) {
       throw error;
     }
@@ -23,12 +31,20 @@ export default class WeathersController {
   public async getWeatherByCoords({ request }: HttpContextContract) {
     const { lon, lat } = request.qs();
     try {
-      const location = await this.locationService.storeLocation({
+      const { id, name } = await this.locationService.storeLocation({
         lon,
         lat,
       });
-      const weather = this.weatherService.getWeatherByName(location.name);
-      return weather;
+      const weather = await this.weatherService.getWeatherByName(name);
+
+      return {
+        location: { id, name },
+        weather: {
+          icon: weather.weather[0].icon,
+          temperature: weather.main.temp,
+          description: weather.weather[0].description,
+        },
+      };
     } catch (error) {
       throw error;
     }
--- start/routes/usersFavoriteCities.ts
+++ start/routes/usersFavoriteCities.ts
@@ -3,9 +3,9 @@ import Route from "@ioc:Adonis/Core/Route";
 export default function usersFavoriteCitiesRoutes() {
   Route.group(() => {
     Route.get("", "UsersFavoriteCitiesController.index").as("usersFavoriteCities.index");
-    Route.post("/:cityName", "UsersFavoriteCitiesController.attach").as(
-      "usersFavoriteCities.attach"
-    );
+    Route.post("/:cityId", "UsersFavoriteCitiesController.attach")
+      .where("cityId", Route.matchers.number())
+      .as("usersFavoriteCities.attach");
     Route.delete("/:cityId", "UsersFavoriteCitiesController.dettach")
       .where("cityId", Route.matchers.number())
       .as("usersFavoriteCities.dettach");
